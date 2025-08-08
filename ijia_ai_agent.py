20d4f7277fe8023063405417126a1ffa4c589497
luccilevi-art <luccilevi@googlemail.com>
2025-08-08 16:13:01 +0100

    Update ijia_ai_agent.py

--- ijia_ai_agent.py
+++ ijia_ai_agent.py
@@ -1,3 +1,4 @@
+
 import streamlit as st
 
 st.set_page_config(page_title="BBSP Eligibility Checker", layout="centered")
@@ -9,7 +10,7 @@ if 'values' not in st.session_state:
     st.session_state.values = {}
 
 def step_1():
-    ans = st.radio("1. Is your business registered and controlled in Jersey?", ["", "Yes", "No"], key="q1")
+    ans = st.radio("1. Is your business registered and controlled in Jersey?", ["Yes", "No"], key="q1")
     if ans == "No":
         st.error("You must be Jersey-registered to qualify.")
         st.stop()
@@ -19,7 +20,7 @@ def step_1():
         st.experimental_rerun()
 
 def step_2():
-    ans = st.radio("2. Do you employ at least one person (owner + 1) paid £128+ per week for 3+ months?", ["", "Yes", "No"], key="q2")
+    ans = st.radio("2. Do you employ at least one person (owner + 1) paid £128+ per week for 3+ months?", ["Yes", "No"], key="q2")
     if ans == "No":
         st.error("Your business must meet the employment condition to qualify.")
         st.stop()
@@ -29,7 +30,7 @@ def step_2():
         st.experimental_rerun()
 
 def step_3():
-    ans = st.radio("3. Are your tax/social obligations up to date or on an adhered payment plan for 3+ months?", ["", "Yes", "No"], key="q3")
+    ans = st.radio("3. Are your tax/social obligations up to date or on an adhered payment plan for 3+ months?", ["Yes", "No"], key="q3")
     if ans == "No":
         st.error("Tax/Social obligations must be current or on an agreed plan.")
         st.stop()
@@ -39,14 +40,14 @@ def step_3():
         st.experimental_rerun()
 
 def step_4():
-    ans = st.selectbox("4. How long has your business been operating?", ["", "Under 6 months", "6 to 18 months", "More than 18 months"], key="q4")
-    if ans:
+    ans = st.selectbox("4. How long has your business been operating?", ["Select one", "Under 6 months", "6 to 18 months", "More than 18 months"], key="q4")
+    if ans != "Select one":
         st.session_state.values['operation_length'] = ans
         st.session_state.step = 5
         st.experimental_rerun()
 
 def step_5():
-    ans = st.radio("5. Does your business operate in the Visitor Economy?", ["", "Yes", "No"], key="q5")
+    ans = st.radio("5. Does your business operate in the Visitor Economy?", ["Yes", "No"], key="q5")
     if ans:
         st.session_state.values['visitor_economy'] = (ans == "Yes")
         st.session_state.step = 6
@@ -55,9 +56,9 @@ def step_5():
 def show_result():
     vals = st.session_state.values
     if vals['operation_length'] == "More than 18 months":
-        max_amount = 75_000
+        max_amount = 75000
     else:
-        max_amount = 5_000
+        max_amount = 5000
 
     st.success("Congratulations! You appear to be eligible.")
     msg = f"Eligible for up to £{max_amount:,} Productivity grant"
